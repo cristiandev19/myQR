198b83c73175ec3248323097150c6b67ac873b07
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-06 00:49:58 -0500

    ultimo index que no es necesario

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 require('dotenv').config();
 
-
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
-// Set the region
+// Set the region 
 AWS.config.update({region: process.env.AWS_S3_REGION});
 
 // Create S3 service object
 s3 = new AWS.S3() ;
-// s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-// call S3 to retrieve upload file to specified bucket
+// Create params for S3.deleteBucket
+var bucketParams = {
+  Bucket : process.env.AWS_S3_BUCKET_NAME
+};
+
 var uploadParams = {
   Bucket: process.env.AWS_S3_BUCKET_NAME,
   Key: '',
-  Body: ''
+  Body: '',
+  ACL          : 'public-read', // para poder ser leido publicamente
+  StorageClass : 'REDUCED_REDUNDANCY' // para poder ser leido publicamente
 };
 var file = './utilities/prueba.png';
-
-// Create the parameters for calling createBucket
-var bucketParams = {
-  Bucket : 'pruebacreacion'
-};
-
-
-
 // Configure the file stream and obtain the upload parameters
 var fs = require('fs');
 var fileStream = fs.createReadStream(file);
@@ -33,13 +29,15 @@ fileStream.on('error', function(err) {
 });
 uploadParams.Body = fileStream;
 var path = require('path');
-uploadParams.Key = path.basename(file);
+uploadParams.Key = 'archivo1.png';
+// console.log('uploadParams.Key', uploadParams.Key)
 
-// call S3 to retrieve upload file to specified bucket
-s3.upload (uploadParams, function (err, data) {
-  if (err) {
-    console.log("Error", err);
-  } if (data) {
-    console.log("Upload Success", data.Location);
-  }
+
+s3.putObject(uploadParams, function(err, data) {
+  console.log('111111111')
+  console.log('err', err)
+  console.log('data', data)
+
+  // const url = `${global.__S3_PATH}${bucket}/${key}`;
+  // console.log('url', url)
 });
