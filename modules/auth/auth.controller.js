62122f77b089303c32b3b2fd7d84d39cc370bb1a
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-09 23:48:57 -0500

    auth controlers

---
+++ modules/auth/auth.controller.js
@@ -0,0 +1,36 @@
+const User = require("../../models/User");
+
+exports.emailLogin = (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    return next(error);
+  }
+}
+
+exports.emailSignup = (req, res, next) => {
+  try {
+    const { names, lastNames, email, password } = req.body;
+    // const user =
+    const user = new User({ names, lastNames, email, password });
+    user.save();
+    console.log('user', user)
+    return res.status(200).send({
+      message: 'Funciono'
+    })
+  } catch (error) {
+    return next(error);
+  }
+}
+
+// const userSchema = new mongoose.Schema({
+//   names              : String,
+//   lastNames          : String,
+//   email              : { type: String, unique: true },
+//   password           : String,
+//   // Cuando quieres cambiar la contraseña, se usa el Token
+//   passwordResetToken : String,
+//   emailVerified      : Boolean
+// }, { timestamps: true });
+
+
