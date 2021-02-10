3f2f235c15aef70234a5b5bcbc23cdd4ace40839
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-09 23:09:24 -0500

    arreglando la variable

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const config = {
   dev          : process.env.NODE_ENV !== 'production',
   port         : process.env.PORT || 3000,
   mongoConnect : process.env.MONGO_CONNECT,
-  jwtSecretKey : JWT_SECRET_KEY
+  jwtSecretKey : process.env.JWT_SECRET_KEY
 };
 
 module.exports = { config };
