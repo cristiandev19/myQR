80f9d278258030d464d0b6fa3d7628d8786ef50d
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-11 23:31:45 -0500

    mas variables

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
 const config = {
-  dev          : process.env.NODE_ENV !== 'production',
-  port         : process.env.PORT || 3000,
-  mongoConnect : process.env.MONGO_CONNECT,
-  jwtSecretKey : process.env.JWT_SECRET_KEY
+  dev             : process.env.NODE_ENV !== 'production',
+  port            : process.env.PORT || 3000,
+  mongoConnect    : process.env.MONGO_CONNECT,
+  jwtSecretKey    : process.env.JWT_SECRET_KEY,
+  awsS3BucketName : process.env.AWS_S3_BUCKET_NAME,
+  awsS3Region     : process.env.AWS_S3_REGION
 };
 
 module.exports = { config };
