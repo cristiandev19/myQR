33f8e337b53fb78fd8f5e0d471f22e96af218d3a
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-13 00:25:22 -0500

    actualizo variables

--- config/index.js
+++ config/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 const config = {
-  dev             : process.env.NODE_ENV !== 'production',
-  port            : process.env.PORT || 3000,
-  mongoConnect    : process.env.MONGO_CONNECT,
-  jwtSecretKey    : process.env.JWT_SECRET_KEY,
-  awsS3BucketName : process.env.AWS_S3_BUCKET_NAME,
-  awsS3Region     : process.env.AWS_S3_REGION
+  dev               : process.env.NODE_ENV !== 'production',
+  port              : process.env.PORT || 3000,
+  mongoConnect      : process.env.MONGO_CONNECT,
+  jwtSecretKey      : process.env.JWT_SECRET_KEY,
+  awsS3BucketName   : process.env.AWS_S3_BUCKET_NAME,
+  awsS3Region       : process.env.AWS_S3_REGION,
+  authJwtSecret     : process.env.AUTH_JWT_SECRET,
+  authJwtExpireTime : process.env.AUTH_JWT_EXPIRE_TIME
 };
 
 module.exports = { config };
