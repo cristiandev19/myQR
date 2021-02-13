77eb274e62ecc7fc378610f87c97e10a45e3b1da
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-13 00:25:29 -0500

    aun no esta probado

--- modules/admin/admin.controller.js
+++ modules/admin/admin.controller.js
@@ -1,47 +1,35 @@
 const { config } = require('../../config/index');
 const imagesUtilities = require('../../utilities/images');
+const qrUtilities = require('../../utilities/qr');
 const awsUtilities = require('../../utilities/aws');
 
 exports.uploadFile = async (req, res, next) => {
   try {
     const { extension, dataFile, fileName } = req.body;
 
-    const bufferObject = await imagesUtilities.convertBase64ToBuffer(dataFile);
+    const bufferObjectImg = await imagesUtilities.convertBase64ToBuffer({
+      fileName,
+      extension,
+      bufferBody: bufferObjectImg.buffer,
+      contentType: bufferObjectImg.contentType
+    });
 
-    // call S3 to retrieve upload file to specified bucket
-    const uploadParams = {
-      Bucket          : config.awsS3BucketName,
-      ACL             : 'public-read',                // para poder ser leid3_BUCKET_NAME,
-      Key             : `${fileName}.${extension}`,
-      Body            : bufferObject.buffer,
-      ACL             : 'public-read',                // para poder ser leido publicamente
-      StorageClass    : 'REDUCED_REDUNDANCY',         // para poder ser leido publicamente
-      ContentType     : bufferObject.contentType,
-      ContentEncoding : 'base64',
-    };
-    const uploaded = await awsUtilities.s3UploadPromise(uploadParams);
+    const uploadedImg = await awsUtilities.s3UploadPromise(uploadParams);
     console.log('uploaded', uploaded);
 
     // const file = '../q.png';
+    const { dataQR } = await qrUtilities.generateQRByUrl(uploadedImg.Location);
+    const bufferObjectQR = await imagesUtilities.convertBase64ToBuffer(dataQR);
 
-    // // Configure the file stream and obtain the upload parameters
-    // var fs = require('fs');
-    // var fileStream = fs.createReadStream(file);
-    // fileStream.on('error', function(err) {
-    //   console.log('File Error', err);
-    // });
-    // uploadParams.Body = fileStream;
-    // var path = require('path');
-    // uploadParams.Key = 'qr_prueba.png';
+    const fileNameQR = `qr__${fileName}__`;
+    const extensionQR = bufferObjectQR.type;
 
-    // // call S3 to retrieve upload file to specified bucket
-    // s3.upload(uploadParams, function (err, data) {
-    //   if (err) {
-    //     console.log("Error", err);
-    //   } if (data) {
-    //     console.log("Upload Success", data.Location);
-    //   }
-    // });
+    const uploaded = await awsUtilities.s3UploadPromise({
+      fileName: fileNameQR,
+      extension: extensionQR,
+      bufferBody: bufferObjectQR.buffer,
+      contentType: bufferObjectQR.contentType
+    });
 
     console.log('hola', req.body.extension);
     return res.status(200).send({
