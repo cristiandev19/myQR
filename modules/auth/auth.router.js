f7d5f4a0acbffd98109c26792037617c7e23deb4
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-14 22:20:16 -0500

    rutas

--- modules/auth/auth.router.js
+++ modules/auth/auth.router.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+// //passport stuff
+const passport      = require('passport');
+
+
 const auth_controller = require('./auth.controller');
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
+  console.log('Auth Time: ', Date.now());
   next();
 });
 
 router
-.get('/emailLogin', auth_controller.emailLogin)
-.post('/emailSignup', auth_controller.emailSignup);
+.post('/email-login', auth_controller.emailLogin)
+.post('/email-signup', auth_controller.emailSignup)
+.get('/protected', passport.authenticate('jwt', { session: false }), auth_controller.protected);
 
 module.exports = router;
