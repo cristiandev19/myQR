83bab609a316f2770f7330fbae504489ae6eb8c6
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-14 22:20:26 -0500

    jwt strategu

--- strategies/jwt.js
+++ strategies/jwt.js
@@ -1,16 +1,29 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const { config } = require('../config/index');
-const opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = config.authJwtSecret; //normally store this in process.env.secret
+const User = require('../models/User');
+
+const opts = {
+  jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey : config.jwtSecretKey //normally store this in process.env.secret
+};
+console.log('opts', opts);
 
 // Aqui debemos cambiarlo para que llame a la BD por un id dentro del token
-module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
-  if (jwt_payload.email === "paul@nanosoft.co.za") {
-    return done(null, true)
-  } else if (jwt_payload.email === 'cristiansotomayor.dev@gmail.com') {
-    return done(null, true)
-  }
-  return done(null, false)
+module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
+  console.log('jwt_payload', jwt_payload);
+  // return done(null, true)
+
+  User.findOne({_id: jwt_payload.sub}, (err, user) => {
+    console.log('user', user);
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false)
+    }
+
+    return done(null, user)
+  });
+
 })
